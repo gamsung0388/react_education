450f87e420aa35130bc60c1ad999e3b1f9b108d4
gamsung0388 <82205370+gamsung0388@users.noreply.github.com>
2023-09-20 16:15:22 +0900

    게시판 등록,수정, 삭제, 목록, 상세 작업

--- boardTest/src/main/java/com/example/boardTest/Board/BoardController.java
+++ boardTest/src/main/java/com/example/boardTest/Board/BoardController.java
@@ -2,6 +2,7 @@ package com.example.boardTest.Board;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -12,7 +13,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.boardTest.db.BoardEntity;
-import com.example.boardTest.db.BoardMapper;
 import com.example.boardTest.dto.BoardSaveDto;
 import com.example.boardTest.util.Header;
 import com.example.boardTest.util.Search;
@@ -21,6 +21,7 @@ import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @RestController
+@CrossOrigin(origins="http://localhost:3030")
 public class BoardController {
 	private final BoardService boardService;
 
@@ -33,6 +34,7 @@ public class BoardController {
     //idx의 데이터 1개를 조회한다.
     @GetMapping("/board/{idx}")
     Header<BoardEntity> getBoardOne(@PathVariable Long idx) {
+    	System.out.println("1:                 " + boardService.getBoardOne(idx));
         return boardService.getBoardOne(idx);
     }
 
